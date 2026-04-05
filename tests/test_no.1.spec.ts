f761743be611a0b091673fad1770e9c86a36b257
DoubleNat <127852220+DoubleNat@users.noreply.github.com>
2026-04-05 19:30:37 +0700

    Add test for finding duplicates and sorting

---
+++ tests/test_no.1.spec.ts
@@ -0,0 +1,10 @@
+import test from "@playwright/test";
+import { findDuplicatesAndSort } from "../resources/test_1";
+
+test("Test No.1: Find Duplicate and Sort", async () => {
+    const listA = [5, 9, 6, 8, 2, 1];
+    const listB = [1, 2, 3, 4, 7, 9];
+   
+    const duplicatedAndSortedList = findDuplicatesAndSort(listA,listB);
+    console.log("List: " + duplicatedAndSortedList);
+})
